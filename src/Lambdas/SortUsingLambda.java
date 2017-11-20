cd2aba9fda7ab44d3c4e71fe649441aac9a20cce
Md Farhan Khan <khanfarhan281@gmail.com>
2017-11-20 22:53:22 +0530

    Halfway through explaination details

--- src/Lambdas/SortUsingLambda.java
+++ src/Lambdas/SortUsingLambda.java
@@ -2,6 +2,12 @@ package Lambdas;
 
 import java.util.*;
 
+/***
+ * This example shows how we can sort a Collection using lambdas.
+ * Comparator is a functional interface in Java so we can use lambdas instead of creating anonymous class
+ * I have also included the use of forEach() which has been introduced in the Collections framework
+ */
+
 public class SortUsingLambda {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
@@ -11,7 +17,8 @@ public class SortUsingLambda {
         people.add(new Person("Prabuddh",23,'m'));
         people.add(new Person("Kislay",24,'m'));
 
-        //Older way to sort a Collection
+        //Older way to sort a Collection using Collection.sort() and passing an object of anonymous class to it
+        //Here we are sorting according to the lexicographical order of names
         Collections.sort(people, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
@@ -19,19 +26,23 @@ public class SortUsingLambda {
             }
         });
 
-        //Sort according to lexicographic order of names
+        //Sort according to lexicographic order of names in Java 8
+        //We just pass a lambda instead of creating an anonymous class
         Collections.sort(people,(Person a,Person b) ->  a.getName().compareTo(b.getName()));
         System.out.println("Sorted according to name lexicographically");
-        people.stream().forEach(System.out::println);
+
+        //Printing all the elements of the Collection using forEach()
+        //System.out::println is a method reference
+        people.forEach(System.out::println);
 
         //Sort according to length of names
         Collections.sort(people, (a,b) -> Integer.compare(a.getName().length(), b.getName().length()));
         System.out.println("Sorted according to name length");
-        people.stream().forEach(System.out::println);
+        people.forEach(System.out::println);
 
         //Sort according to age
         people.sort( (a,b) -> Integer.compare(a.getAge(), b.getAge()));
         System.out.println("Sorted according to age");
-        people.stream().forEach(System.out::println);
+        people.forEach(System.out::println);
     }
 }
